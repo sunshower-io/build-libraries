f42f823244470b7d0c12d668297a24db59814503
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-23 18:18:26 -0600

    testing

--- vars/skipRelease.groovy
+++ vars/skipRelease.groovy
@@ -13,12 +13,12 @@ def check() {
     result = sh (script: "git log -1 | grep '.*\\[skip-build\\].*'", returnStatus: true)
     if (result == 0) {
         env.SKIP_BUILD = "true"
-        echo "'[skip-build]' found in git commit message. Aborting."
+        error "'[skip-build]' found in git commit message. Aborting."
     }
 }
 
 def postProcess() {
     if (env.SKIP_BUILD) {
-        currentBuild.result = 'NOT_BUILT'
+        currentBuild.result = 'SUCCESS'
     }
 }
