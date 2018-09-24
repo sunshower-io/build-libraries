1ce32105e448b0ff9727f024a4ef9fc6aa08b7c4
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-24 00:13:58 -0600

    update

--- vars/extractVersions.groovy
+++ vars/extractVersions.groovy
@@ -13,9 +13,8 @@ def extractFromFile(String text) {
     env.CURRENT_VERSION = segs.join('.')
     env.NEXT_VERSION = "${segs.join('.')}.Final"
 
-    def nextsegs = segs.collect()
-    nextsegs[nextsegs.length -1]++
-    env.NEXT_SNAPSHOT = "${nextsegs}-SNAPSHOT"
+    segs[segs.length -1]++
+    env.NEXT_SNAPSHOT = "${segs}-SNAPSHOT"
 
     echo "\t Current Version: $env.CURRENT_VERSION-SNAPSHOT"
     echo "\t Next Version: $env.NEXT_VERSION"
