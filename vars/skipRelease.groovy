56b4d51701ba975a5cf922c79185c39ab05a85eb
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-23 18:29:07 -0600

    Trying non-exception force

--- vars/skipRelease.groovy
+++ vars/skipRelease.groovy
@@ -1,24 +1,30 @@
 def call(Map args) {
     if (args.action == 'check') {
-        return check()
+        return check(args.forceAbort)
     }
     if (args.action == 'postProcess') {
-        return postProcess()
+        return postProcess(args.forceAbort)
     }
     error 'skip has been called without valid arguments'
 }
 
-def check() {
-    env.SKIP_BUILD = false; 
-    result = sh (script: "git log -1 | grep '.*\\[skip-build\\].*'", returnStatus: true)
+def check(boolean forceAbort) {
+    env.SKIP_BUILD = false;
+    result = sh(script: "git log -1 | grep '.*\\[skip-build\\].*'", returnStatus: true)
     if (result == 0) {
-        env.SKIP_BUILD = "true"
-        error "'[skip-build]' found in git commit message. Aborting."
+        env.SKIP_BUILD = true
+        if (forceAbort) {
+            error "'[skip-build]' found in git commit message. Aborting."
+        } else {
+            echo "'[skip-build]' was found in git commit message.  Setting environment"
+        }
     }
 }
 
-def postProcess() {
+def postProcess(forceAbort) {
     if (env.SKIP_BUILD) {
-        currentBuild.result = 'SUCCESS'
+        if (forceAbort) {
+            currentBuild.result = 'NOT_BUILT'
+        }
     }
 }
