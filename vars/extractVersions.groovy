6d2291d4cc5b3aea4a97b494ea818f68aac52a83
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-24 00:17:59 -0600

    update

--- vars/extractVersions.groovy
+++ vars/extractVersions.groovy
@@ -9,15 +9,13 @@ def call(Map args) {
 def extractFromFile(String text) {
     echo "Extracting variables from: $text"
     def segs = text.split('\\-')[0].split('\\.').collect{ t -> Integer.valueOf(t) }
-    echo "Segs: $segs"
     env.CURRENT_VERSION = segs.join('.')
     env.NEXT_VERSION = "${segs.join('.')}.Final"
 
-    echo "Segs2: $segs"
-//    segs[segs.length -1]++
+    segs[segs.size() -1]++
     env.NEXT_SNAPSHOT = "${segs}-SNAPSHOT"
 
-    echo "\t Current Version: $env.CURRENT_VERSION-SNAPSHOT"
+    echo "\t Current Version: $env.CURRENT_VERSION"
     echo "\t Next Version: $env.NEXT_VERSION"
     echo "\t Next SNapshot : $env.NEXT_SNAPSHOT"
 }
