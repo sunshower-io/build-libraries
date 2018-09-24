9002d7ed3cabd9b8c8e37ea832eaaecdba9af8a6
Josiah Haswell <josiah.haswell@gmail.com>
2018-09-24 00:05:48 -0600

    update

--- vars/extractVersions.groovy
+++ vars/extractVersions.groovy
@@ -6,31 +6,12 @@ def call(Map args) {
     }
 }
 
-def segments(String v) {
-    def a1 = v.split('\\-')
-    echo "a1: $a1"
-    def a2 = a1[0]
-    echo "a2: $a2"
-    def a3 = a2.split('\\.')
-    echo "a3: $a3"
-    def a4 = a3.collect{t ->
-        echo "t: $t"
-        return Integer.parseInt(t)
-    }
-    echo "$a4"
-    return a4
-//    return v.split('\\-')[0].split('\\.').collect{ t -> Integer.valueOf(t) }
-}
-
-@NonCPS
 def extractFromFile(String text) {
     echo "Extracting variables from: $text"
-    final def segs = segments(text)
-    echo "1"
+    def segs = v.split('\\-')[0].split('\\.').collect{ t -> Integer.valueOf(t) }
+    echo "Segs: $segs"
     env.CURRENT_VERSION = segs.join('.')
-    echo "2"
     env.NEXT_VERSION = "${segs.join('.')}.Final"
-    echo "3"
     env.NEXT_SNAPSHOT = "${increment(segs)}-SNAPSHOT"
 
     echo "\t Current Version: $env.CURRENT_VERSION-SNAPSHOT"
