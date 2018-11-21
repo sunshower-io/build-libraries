67308e942c1898505b754a552ce9624794aec749
Josiah Haswell <josiah.haswell@gmail.com>
2018-11-20 21:20:55 -0700

    Adding email

--- vars/sendEmails.groovy
+++ vars/sendEmails.groovy
@@ -1,3 +1,3 @@
 def call(Map args) {
-    emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'josiah'
+    emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'josiah@sunshower.io'
 }
